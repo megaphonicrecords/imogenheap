a083be9877e9c6e44e5af5cf8cdb4e7a21170df6
Jacob <jacobhulmston@gmail.com>
2024-01-29 17:05:37 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inconsolata.className}>{children}
-      <Footer />
+      <Footer/>
       </body>
     </html>
   )
