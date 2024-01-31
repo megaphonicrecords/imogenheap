5388d0c5a95d5583456c4c79e651d2ded09e7683
Jacob <jacobhulmston@gmail.com>
2024-01-31 19:32:49 +0000

    Style: Wordmark animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,16 @@ const config: Config = {
         // Add more colors as needed
       },
     },
+    keyframes: {
+      fadeIn: {
+        '0%': { opacity: '0' },
+        '100%': { opacity: '1' },
+      },
+    },
+    animation: {
+      fadeIn: 'fadeIn 0.5s ease-in-out',
+    },
+
   },
   variants: {
     extend: {
